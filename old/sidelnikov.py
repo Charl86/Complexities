f388a5fb57e729672659c3fe8c0347548a1819ed
Charl86 <carl.hernan34@gmail.com>
2021-03-23 20:22:31 -0400

    First commit.

---
+++ old/sidelnikov.py
@@ -0,0 +1,17 @@
+
+class Sidelnikov:
+    def __init__(self, q):
+        self.q = q
+
+        self.galoisFieldq = GF(self.q, 'a')
+
+        if not is_prime_power(self.q):
+            raise TypeError(f"{self.q} must be a prime or a prime power.")
+
+    def __call__(self, t):
+        primitive_elm = self.galoisFieldq.primitive_element()
+
+        if not (primitive_elm^t + 1).is_square():
+            return 1
+        else:
+            return 0
